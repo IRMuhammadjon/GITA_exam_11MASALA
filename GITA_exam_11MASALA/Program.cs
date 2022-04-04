51a45d457118db57dd86e45604e22df9993be4a7
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-04-04 09:31:49 +0500

    Добавьте файлы проекта.

---
+++ GITA_exam_11MASALA/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Bo'linuvchini kiriting: ");
+var B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Bo'luvchini kiriting: ");
+var b= Convert.ToInt32(Console.ReadLine());
+int count=0;
+while (B > 0)
+{
+    B = B - b;
+   count++;
+}
+if (B != 0)
+{
+    B = B + b;
+    count--;
+}
+Console.WriteLine("Qoldiq: "+ B);
+Console.WriteLine("Butun qismi: "+ count);
